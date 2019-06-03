de2b8002eaae1e80df58dc58d16c7a58109df019
Aaron Hurst <aahurst@au1.ibm.com>
2019-06-03 14:33:30 +0800

    Updated main app and requirements

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template#, flash, redirect, request, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('search.html')
+
+@app.route('/results', methods=['POST'])
+def get_results():
+
+    # Validate the query string
+    # length? special characters?
+    natural_language_query = request.form.get('natural_language_query')
+
+    # Run query
+
+    # Format results
+    # get doc title, passage(s), page numbers?
+    # format into appropriate dictionary
+
+    # Render template ... with fields: title + passage for documents, original search query should be in search box (different colour or something)
+    print(request.form.get('natural_language_query'))
+    return render_template('results.html')
 
 if __name__=='__main__':
     app.run(debug=True)
